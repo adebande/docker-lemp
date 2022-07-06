4aac40f4b67987cad954837559222c6df5520b6f
Aloïs Debande <63793809+adebande@users.noreply.github.com>
2022-07-06 10:27:05 +0200

    test pipeline

--- app/index.php
+++ app/index.php
@@ -1,14 +1,3 @@
 <?php
-$servername   = "db:3306";
-$database = "db";
-$username = "user";
-$password = "password";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
-}
-  echo "Connected successfully";
+echo "Test Pipeline";
 ?>
